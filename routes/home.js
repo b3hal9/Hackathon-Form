642bd059757bc938a72152d6617311b3edaa74ec
b3hal9 <b3hal9@gmail.com>
2020-01-08 22:32:00 +0545

    dummy version 2

--- routes/home.js
+++ routes/home.js
@@ -3,15 +3,42 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 
-router.get('/', (req,res)=>{
-    res.sendFile("../views/index" , {root: __dirname});
+const {body, validationResult} = require('express-validator/check');
+const {matchedData,sanitizeBody} = require('express-validator/filter');
+
+router.get('/',(req,res)=>{
+    res.sendFile('../src/index',{root:__dirname});
+})
+router.post('/register',[
+    body('username', 'invalid username').isLength({min:6}).not().isEmpty(),
+    body('email', 'invalid email.').isEmail().normalizeEmail().not().isEmpty(),
+    body('phone','invalid number').isLength({min:10 , max:10}).not().isEmpty(),
+    body('faculty', 'choose your faculty').not().isEmpty(),
+    body('sex', 'mention your sex.').not().isEmpty(),
+    body('class','class is required').not().isEmpty(),
+    body('field', 'mention your field.').not().isEmpty(),
+    // body('feedback','feedback is required').not().isEmpty(),
+    body('agreement', 'check the box.').not().isEmpty(),
+   
+
+], (req,res)=>{
+   
+    const errors = validationResult(req);
+    console.log(errors.mapped());
+   const data =matchedData(req);
+
+if(!errors.isEmpty()){
+    // return res.status(422).json({errors: errors.mapped()});
+
+    res.render('../views/index',{title: 'inv        alid data', error:errors.mapped(),data:data});
+    
+}else{
+res.render("../views/register" , {title: 'Data Saved',
+        message: 'Thanks, Your Registration is Successful.'});
+        console.log(req.body);
+    }
 })
 
-router.post('/register', async(req, res) => {
-    
-    await res.render('index', {title: 'Data Saved',
-    message: 'Thankyou for your Registration'});
-    console.log(req.body);
-})
+
 
 module.exports = router;
